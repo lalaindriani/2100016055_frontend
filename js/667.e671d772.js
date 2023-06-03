1964f7e0f20020e83826b503b065a7f945473dca
lalair26 <rindri327@gmail.com>
2023-06-04 00:37:10 +0700

    first commit

---
+++ js/667.e671d772.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_dashboard"]=self["webpackChunkvue_dashboard"]||[]).push([[667],{5667:function(e,n,r){r.r(n),r.d(n,{default:function(){return d}});var t=r(3396);const a={class:"font-bold text-4xl py-8",style:{"font-family":"Georgia"}};function s(e,n,r,s,u,o){return(0,t.wg)(),(0,t.iD)("div",a,"User")}var u={},o=r(89);const c=(0,o.Z)(u,[["render",s]]);var d=c}}]);
+//# sourceMappingURL=667.e671d772.js.map
